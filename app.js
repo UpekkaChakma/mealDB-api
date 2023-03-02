9f932a9d37959eb572e00d299ba41259233cb079
Upekka Chakma <upekkachakma@gmail.com>
2023-03-02 15:00:25 +0600

    takes letter input, made a function that warns

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const noResultDiv = document.getElementById("noResultDiv");
 const mealDiv = document.getElementById("meal-details-div");
 const input = document.getElementById("inputText");
 
+
+function warn(text) {
+    mealDiv.classList = "d-none";
+    searchedMealsList.classList = 'd-none';
+    noResultDiv.innerHTML = `
+    <h4 class="d-block text-center text-danger">
+        ${text}
+    </h4>`
+}
+
 // search meals by first letter
 const searchMealByLetter = () => {
     searchedMealsList.innerHTML = '';
     mealDiv.innerHTML = '';
+    const hasLetterOnly = input.value.match(/^[A-Za-z]+$/);
 
     if (input.value.length > 1) {
-        mealDiv.classList = "d-none";
-        searchedMealsList.classList = 'd-none';
-        noResultDiv.innerHTML = `
-        <h4 class="d-block text-center text-danger">
-            Only single character allowed
-        </h4>`;
+        warn("Only single character allowed");
     }
-    else if (input.value.length === 0) {
-        mealDiv.classList = "d-none";
-        searchedMealsList.classList = "d-none";
-        noResultDiv.innerHTML = `
-        <h4 class="d-block text-center text-danger">
-            Please input a letter
-        </h4>`;
+    else if (input.value.length === 0 || hasLetterOnly === null) {
+        warn("Please input a letter");
     } else {
         noResultDiv.innerHTML = '';
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${input.value}`;
@@ -53,23 +54,23 @@ const displaySearchedResult = (meals) => {
 
 
 
+
+
 // showing clicked meal ingredients
 const mealIngredient = (mealName) => {
+    mealDiv.classList = "d-flex";
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
     fetch(url)
         .then(response => response.json())
         .then(data => mealIngredientDetails(data.meals[0]));
 }
 
-
-
-
 const mealIngredientDetails = (meal) => {
-    mealDiv.classList = "d-flex";
     mealDiv.innerHTML = `
         <img src = "${meal.strMealThumb}">
         <div>
             <h2>${meal.strMeal}</h2>
+            <h5>${meal.strArea} ${meal.strCategory}</h5>
             <ul>
                 <li>${meal.strIngredient1}</li>
                 <li>${meal.strIngredient2}</li>
@@ -77,9 +78,9 @@ const mealIngredientDetails = (meal) => {
                 <li>${meal.strIngredient4}</li>
                 <li>${meal.strIngredient5}</li>
                 <li>${meal.strIngredient6}</li>
-                <li>${meal.strIngredient7}</li>
-                <li>${meal.strIngredient8}</li>
-                <li>${meal.strIngredient9}</li>
+                <li class=${!meal.strIngredient7 && 'd-none'} >${meal.strIngredient7}</li>
+                <li class=${!meal.strIngredient8 && 'd-none'} >${meal.strIngredient8}</li>
+                <li class=${!meal.strIngredient9 && 'd-none'} >${meal.strIngredient9}</li>
             </ul>
         </div>
     `
